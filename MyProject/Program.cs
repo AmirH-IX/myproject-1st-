7fa333ad5f0742255742a7563ebec327b739fca3
AmirH-IX <amirh.aghdaki@gmail.com>
2022-11-02 19:59:45 +0330

    first commit

---
+++ MyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Boolean
+{
+    public static void Main()
+    {
+        bool content = true;
+        bool nocontent = false;
+        Console.WriteLine("test is {0}", content);
+        Console.WriteLine("test is {0}", nocontent);
+    }
+}
